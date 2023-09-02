149102819a0dc5be074aecf088e8fd5cf506901f
Barsaiyaps <pankajbars@gmail.com>
2023-09-02 18:44:15 +0530

    added prime code

---
+++ prime.js
@@ -0,0 +1,14 @@
+let number =5;
+let fact =0;
+for(let i=1;i<=number;i++)
+{
+    if(number%1==0){
+        fact++
+    }
+}
+if(fact==2){
+    console.log("Prime")
+}
+else{
+    console.log("Not Prime")
+}
